b96cba5e98a0fdd44f3a32ef803be3cf8e66d874
Srijon Paul <86311499+srijonp4@users.noreply.github.com>
2024-05-30 21:40:58 +0530

    fix 2

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -12,7 +12,6 @@ const index = new Index();
 
 async function main() {
   const connector = neon(process.env.DATABASE_URL!);
-  // @ts-expect-error neon-drizzle
   const db = drizzle(connector);
 
   const products: (typeof productsTable.$inferInsert)[] = [];
