169a592a32a360dd89d4803681b58c945ed0f253
Srijon Paul <86311499+srijonp4@users.noreply.github.com>
2024-05-30 21:37:02 +0530

    fix 2

--- src/db/index.ts
+++ src/db/index.ts
@@ -11,5 +11,4 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
 const connector = neon(process.env.DATABASE_URL!);
-// @ts-expect-error
 export const db = drizzle(connector);
